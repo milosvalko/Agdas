6c5811a7ac7581a75eef05281f4d77d4fab714c5
jakubsimek97 <simek.kuba@email.cz>
2022-10-26 11:00:47 +0200

    automatic installation of requirements

---
+++ install_requiremets.py
@@ -0,0 +1,32 @@
+import importlib, os
+"""
+Install requirements for Agdas
+"""
+
+# path of agdas project
+script_path = os.path.dirname(os.path.realpath(__file__))
+
+# create requirements.txt by pipreqs
+os.system('python -m pipreqs.pipreqs {} --encoding utf-8-sig --force'.format(script_path))
+
+# open requirements
+with open('requirements.txt', 'r') as f:
+
+    for l in f.read().splitlines():
+        library = l.split('=')[0]
+
+        # try load the library
+        try:
+            im = importlib.import_module(library)
+            mess = '{} is installed'.format(library)
+            print(mess)
+
+        # install the library if it doesn't
+        except ModuleNotFoundError:
+            print('=======================================')
+            mess = '{} is not installed'.format(library)
+            print(mess)
+            cmd = 'python -m pip install {}'.format(library)
+            print(cmd)
+            os.system(cmd)
+            print('=======================================')
