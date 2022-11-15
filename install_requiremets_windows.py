386afcde9595b7c5f0cad507c6537d6668454254
jakubsimek97 <simek.kuba@email.cz>
2022-11-15 19:24:47 +0100

    simplify of installing requirements

--- install_requiremets_windows.py
+++ install_requiremets_windows.py
@@ -3,61 +3,23 @@ import importlib, os, sys
 Install requirements for Agdas.
 """
 
-upgrade = True
-
 # path of agdas project
 script_path = os.path.dirname(os.path.realpath(__file__))
 
-# upgrade pip
-print('=======================================')
-print('Upgrade pip')
-cmd = '{} -m pip install pip --upgrade'.format(sys.executable)
-print(cmd)
-
-# install pipreqs
-try:
-    importlib.import_module('pipreqs')
-    # create requirements.txt by pipreqs
-    os.system('{} -m pipreqs.pipreqs {} --encoding utf-8-sig --force'.format(sys.executable, script_path))
-except ModuleNotFoundError:
-    print('=======================================')
-    mess = '{} is not installed'.format('pipreqs')
-    print(mess)
-    cmd = '{} -m pip install {}'.format(sys.executable, 'pipreqs')
-    print(cmd)
-    os.system(cmd)
-    print('=======================================')
-
+ins = False
 # open requirements
 with open('requirements.txt', 'r') as f:
 
     for l in f.read().splitlines():
         library = l.split('=')[0]
 
-        # try load the library
         try:
-            im = importlib.import_module(library)
-            mess = '{} is installed'.format(library)
-            print(mess)
+            importlib.import_module(library)
 
-        # install the library if it doesn't
         except ModuleNotFoundError:
-            print('=======================================')
-            mess = '{} is not installed'.format(library)
-            print(mess)
-            cmd = '{} -m pip install {}'.format(sys.executable, library)
-            print(cmd)
-            os.system(cmd)
-            print('=======================================')
+            ins = True
 
-# upgrade libraries
-if upgrade:
-    print('=======================================')
-    print('Upgrading of libraries')
-    with open('requirements.txt', 'r') as f:
-
-        for l in f.read().splitlines():
-            library = l.split('=')[0]
-            print('=======================================')
-            cmd = '{} -m pip install {} --upgrade'.format(sys.executable, library)
-            print(cmd)
+if ins:
+    os.system('{} -m pip install pipreqs'.format(sys.executable))
+    os.system('{} -m pipreqs.pipreqs {} --encoding utf-8-sig --force'.format(sys.executable, script_path))
+    os.system('{} -m pip install -r requirements.txt'.format(sys.executable))
